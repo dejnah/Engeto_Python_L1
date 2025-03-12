5408ee53c341dbd54119191165a8c2f459ef4acf
dejnah <168757297+dejnah@users.noreply.github.com>
2025-03-12 13:04:00 +0100

    Úkol -  spojovani stringu

---
+++ spojovani_stringu.py
@@ -0,0 +1,8 @@
+jmeno = "Lukáš"
+prijmeni = "Dvořák"
+cele_jmeno = jmeno + " " + prijmeni
+delka_jmena = len(cele_jmeno)
+
+print("Celé jméno:", cele_jmeno)
+print("Délka jména:", delka_jmena)
+print(("=" * delka_jmena) + "\n" + cele_jmeno + "\n" + "=" * delka_jmena)
