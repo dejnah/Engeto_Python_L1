308c5e8a51a90f55711910c7ee92909201f8b45c
dejnah <168757297+dejnah@users.noreply.github.com>
2025-03-12 12:36:24 +0100

    Vytvořila jsem jsem soubor pro cvičení 1 -  ciselne_datove_typy.py

---
+++ ciselne_datove_typy.py
@@ -0,0 +1,10 @@
+# 🧠 CVIČENÍ 1 🧠 - číselné datové typy
+# Doplň, tak ať výstup odpovídá hodnotě v komentáři:
+
+print(10 * 10)   #  = 100
+print(9 - 1)     #  = 8
+print(121 / 3)   #  = 40.333
+print(2 ** 5)     #  = 32
+print(17 % 15)   #  = 2
+print(56 // 13)   #  = 4
+print(8 + 8)     #  = 16
